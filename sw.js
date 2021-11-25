17badeea99018e5cfd8eb99a3b7af69098177f49
DanielAgueroKsquaregroup <daniel.aguero@theksquaregroup.com>
2021-11-25 11:31:44 -0600

    files on reposiroty, images showing pwa working

---
+++ sw.js
@@ -0,0 +1,29 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                'index.html',
+                'style.css',
+                'app.js',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      // magic goes here
+      caches.match(event.request).then((resp) => {
+          return(
+              resp ||
+              fetch(event.request).then( (response) => {
+                  return caches.open("v1").then( (cache) => {
+                      cache.put(event.request, response.clone());
+                      return response;
+                  });
+              })
+          );
+      })
+    );
+});
